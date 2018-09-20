05cdab423a001b6360c9773e1656225667647258
Mark Paluch <mpaluch@paluch.biz>
2018-09-20 10:20:33 +0200

    Update lettuce releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,12 +60,12 @@ repositories {
 }
 
 dependencies {
-    compile("org.springframework:spring-core:5.0.4.RELEASE")
+    compile("org.springframework:spring-core:5.0.9.RELEASE")
     compile("io.projectreactor.ipc:reactor-netty:0.7.7.RELEASE") {
         exclude(group = "io.netty", module = "netty-transport-native-epoll")
     }
     compile("io.projectreactor:reactor-core:3.1.7.RELEASE")
-    compile("io.lettuce:lettuce-core:5.0.4.RELEASE") {
+    compile("io.lettuce:lettuce-core:5.1.0.RELEASE") {
         exclude(group = "io.netty")
     }
     compile("org.yaml:snakeyaml:1.17")
