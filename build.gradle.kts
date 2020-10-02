8ec0651f0076bb154a555abb40f607ff0209f6a3
Mark Paluch <mpaluch@paluch.biz>
2020-10-02 15:56:49 +0200

    Upgrade to Gradle 5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.ShadowExtension
-import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
-import com.github.jengelman.gradle.plugins.shadow.internal.JavaJarExec
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.robfletcher.compass.CompassExtension
-import com.github.robfletcher.compass.CompassPlugin
-import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
-import org.gradle.jvm.tasks.Jar
-import org.gradle.plugins.signing.SigningPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
+import java.util.concurrent.TimeUnit
 
 buildscript {
 
@@ -23,8 +16,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.3")
-        classpath("com.github.robfletcher:compass-gradle-plugin:2.0.6")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-allopen:${kotlinVersion}")
     }
@@ -37,14 +28,15 @@ configurations.all {
 }
 
 plugins {
+    id("com.github.anbuck.compass") version "2.0.7"
     java
-    kotlin("jvm")
+    kotlin("jvm") version "1.3.72"
     application
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
-apply {
-    plugin<CompassPlugin>()
-    plugin<ShadowPlugin>()
+configure<ApplicationPluginConvention> {
+    mainClassName = "io.lettuce.Application"
 }
 
 group = "io.lettuce"
@@ -63,13 +55,8 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<ShadowJar> {
-    archiveName = "lettuce-home-all.jar"
+   archiveFileName.set("lettuce-home-all.jar")
 }
-
-application {
-    mainClassName = "io.lettuce.Application"
-}
-
 
 configure<CompassExtension> {
     sassDir = file("$projectDir/src/main/sass")
@@ -86,9 +73,9 @@ repositories {
 
 dependencies {
 
-    var kotlinVersion = "1.2.61"
+    var kotlinVersion = "1.3.72"
 
-    compile(kotlin("stdlib-jre8", kotlinVersion))
+    compile(kotlin("stdlib-jdk8", kotlinVersion))
     compile(kotlin("reflect", kotlinVersion))
 
     compile("org.springframework:spring-core:5.1.0.RELEASE")
